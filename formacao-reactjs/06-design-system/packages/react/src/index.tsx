f7896499b65252aae09f15a9de7043196cd22685
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-29 11:02:56 -0300

    create avatar story

--- formacao-reactjs/06-design-system/packages/react/src/index.tsx
+++ formacao-reactjs/06-design-system/packages/react/src/index.tsx
@@ -32,3 +32,4 @@ export type ButtonProps = ComponentProps<typeof Button>
 export * from './components/Box'
 export * from './components/Text'
 export * from './components/Heading'
+export * from './components/Avatar'
