b9d8024b7beb6a625096275f48c37d52f1085135
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-29 11:21:44 -0300

    create button story

--- formacao-reactjs/06-design-system/packages/docs/src/stories/Button.stories.tsx
+++ formacao-reactjs/06-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -1,19 +1,60 @@
 import type { StoryObj, Meta } from '@storybook/react'
 import { Button, type ButtonProps } from '@ignite-ui/react'
+import { ArrowRightIcon } from '@phosphor-icons/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
   args: {
-    children: 'Button'
+    disabled: false,
+    children: 'Send',
+    size: 'md'
   },
+  argTypes: {
+    size: {
+      options: ['sm', 'md'],
+      control: {
+        type: 'select'
+      },
+    }
+  }
 } as Meta<ButtonProps>
 
-export const Primary: StoryObj<ButtonProps> = {
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new'
+  }
 }
 
-export const Big: StoryObj<ButtonProps> = {
+export const Tertiary: StoryObj<ButtonProps> = {
   args: {
-    size: 'big'
+    variant: 'tertiary',
+    children: 'Cancel'
+  }
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  }
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        With Icon
+        <ArrowRightIcon weight='bold'  />
+      </>
+    )
+  }
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
   }
 }
