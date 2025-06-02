734bcae22f36f0d8b5c9bc445e4dc2934a0711f0
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-06-02 10:00:12 -0300

    create checkbox story

---
+++ formacao-reactjs/06-design-system/packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { CheckIcon } from '@phosphor-icons/react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { ComponentProps } from 'react'
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {}
+
+export const Checkbox = (props: CheckboxProps) => {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <CheckIcon weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
